981c48e746da7acc61dd85b45f09fa74a4071c21
ARCADIA\kirill.kutepov <kkutepovgg@gmail.com>
2018-10-19 17:58:30 +0300

    Added step wizard logic; improved viewings; added some pictures

--- app/js/script.js
+++ app/js/script.js
@@ -1,26 +1,115 @@
 $(document).ready(function () {
-    try {
-        $('.dropdown-trigger').dropdown();
-    } catch {
-
-    }
+    $('select').formSelect();
 
     $(".step").hide();
     $("#step1").show();
     $(".nextpage").click(nextPage);
     $("[data-group]").click(selectButtonInGroup);
+
+    // var groups = [];
+    // $("[data-group]").each((index, item) => {
+    //     var g = $(item).attr("data-group");
+    //     if (groups.indexOf(g) < 0) {
+    //         groups.push(g);
+    //     }
+    // });
+    // console.log(JSON.stringify(groups.map(item => {
+    //     return { group: item, items: [] };
+    // })));
 });
 
 function nextPage(event, sender) {
-    stepId = $(event.currentTarget).parent().parent().parent().parent().parent().attr("id").slice(-1);
-    // $parent.css( "background", "yellow" )
+    var stepId = $(event.currentTarget).parent().parent().parent().parent().parent().attr("id").slice(-1);
     $("#step" + stepId).hide();
     $("#step" + (++stepId)).show();
 }
 
 function selectButtonInGroup(event, sender) {
-    group = $(event.currentTarget).attr("data-group");
+    var group = $(event.currentTarget).attr("data-group");
     $("[data-group='" + group + "']").removeClass("selected");
     $(event.currentTarget).addClass("selected");
-    console.log(group);
+
+    var target = $(event.currentTarget).attr("data-target");
+    var index = $(event.currentTarget).attr("data-index");
+    var toHide = $(event.currentTarget).attr("data-area-hide");
+    var toShow = $(event.currentTarget).attr("data-area-show");
+
+    var price = groups.find(x => x.group === group).items[index];
+    $("#" + target).css("background-image", `url("images/${target}/${index}.png")`)
+    $("#" + target + "-price").text(price);
+    $("#" + target + "-summary-label").text(price);
+    $("#" + target + "-summary").val(index).formSelect();
+    $("." + toHide).hide();
+    $("." + toShow).show();
+
 }
+
+
+var groups = [{
+    "group": "нужен участок",
+    "items": [
+    ]
+}, {
+    "group": "тип участка",
+    "items": [
+        "от 1 500 000 рублей",
+        "от 1 000 000 рублей",
+        "от 750 000 рублей",
+        "от 500 000 рублей",
+        "от 1 500 000 рублей",
+        "от 1 000 000 рублей",
+        "от 750 000 рублей",
+        "от 500 000 рублей"
+    ]
+}, {
+    "group": "нужен проект",
+    "items": []
+}, {
+    "group": "тип проекта",
+    "items": [
+        "30 000 рублей",
+        "50 000 рублей"
+    ]
+}, {
+    "group": "комплектация",
+    "items": []
+}, {
+    "group": "площадь",
+    "items": []
+}, {
+    "group": "этажность",
+    "items": []
+}, {
+    "group": "дополнительные опции",
+    "items": []
+}, {
+    "group": "разрешение на строительство",
+    "items": [
+        "",
+        "20 000 рублей"
+    ]
+}, {
+    "group": "подключение электричества",
+    "items": [
+        "",
+        "от 5 000 рублей"
+    ]
+}, {
+    "group": "разрешенние на эксплуатацию",
+    "items": [
+        "",
+        "от 5 000 рублей"
+    ]
+}, {
+    "group": "подключение воды",
+    "items": [
+        "",
+        "от 25 000 рублей"
+    ]
+}, {
+    "group": "подключение газа",
+    "items": [
+        "",
+        "от 25 000 рублей"
+    ]
+}];
