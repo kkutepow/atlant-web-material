ba8768f2ec02c0da02b86fcc5e5375784c1e8da2
ARCADIA\kirill.kutepov <kkutepovgg@gmail.com>
2018-10-19 14:55:41 +0300

    Finished design of the whole step wizard (without contact page)

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    try {
+        $('.dropdown-trigger').dropdown();
+    } catch {
 
+    }
+
+    $(".step").hide();
+    $("#step1").show();
+    $(".nextpage").click(nextPage);
+    $("[data-group]").click(selectButtonInGroup);
 });
+
+function nextPage(event, sender) {
+    stepId = $(event.currentTarget).parent().parent().parent().parent().parent().attr("id").slice(-1);
+    // $parent.css( "background", "yellow" )
+    $("#step" + stepId).hide();
+    $("#step" + (++stepId)).show();
+}
+
+function selectButtonInGroup(event, sender) {
+    group = $(event.currentTarget).attr("data-group");
+    $("[data-group='" + group + "']").removeClass("selected");
+    $(event.currentTarget).addClass("selected");
+    console.log(group);
+}
